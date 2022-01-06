3270c52950e4a8e9db8f2f338671f1e87c6434b4
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-06 18:58:54 +0530

    ADDED: X or O Symbol functionality

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,8 +1,9 @@
 package com.bridgelabz;
-
+import java.util.*;
 public class Main {
 
     private static char[] Board = new char[10];
+    private static char playingSybmol;
 
     //UC 1 Method for initializing board position
     private static void initializeBoard() {
@@ -10,10 +11,41 @@ public class Main {
             Board[i]=' ';
         }
     }
+    private static void showBoard() {
+        for (int i = 1; i < 10; i++) {
+            if ((i) % 3 == 0) {
+                System.out.println(Board[i]);
+                if (i != 10 - 1) {
+                    System.out.println("-----");
+                }
+            } else {
+                System.out.print(Board[i] + "|");
+            }
+        }    }
+       //UC 2 Method
+       private static void setPlayingSymbol() {
+           while(playingSybmol != 'X' && playingSybmol != 'O') {
+               System.out.println("Please Select the Symbol for Play: X or O");
+               //Scanner Method for User Input
+               Scanner sc = new Scanner(System.in);
+               playingSybmol = sc.next().charAt(0);
+               switch (playingSybmol) {
+                   case 'X':
+                       System.out.println("Player Selected " + playingSybmol + " Symbol");
+                       break;
+                   case 'O':
+                       System.out.println("Player Selected " + playingSybmol + " Symbol");
+                       break;
+                   default:
+                       System.out.println("Please Select the Symbol From X or O only");
+                       break;
+               }
+           }
+       }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game Simulator.");
 
-        //Initializing the Playing Board boxes
         initializeBoard();
+        setPlayingSymbol();
     }
 }

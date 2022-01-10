a453c35db376dd4940537e8fb10b5f6d1b6ce19a
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-10 15:54:06 +0530

    ADDED: Show Board Functionality

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -11,7 +11,7 @@ public class Main {
             Board[i]=' ';
         }
     }
-    private static void showBoard() {
+    private static void showBoard1() {
         for (int i = 1; i < 10; i++) {
             if ((i) % 3 == 0) {
                 System.out.println(Board[i]);
@@ -42,10 +42,44 @@ public class Main {
                }
            }
        }
+    public static void showBoard() {
+        System.out.println(" ----------- ");
+        System.out.println("| " + Board[1] + " | " + Board[2] + " | " + Board[3] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + Board[4] + " | " + Board[5] + " | " + Board[6] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + Board[7] + " | " + Board[8] + " | " + Board[9] + " |");
+        System.out.println(" ----------- ");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game Simulator.");
 
         initializeBoard();
         setPlayingSymbol();
+        showBoard1();
     }
 }
+
+
+
+/*private static char[] boardArray= new char[10];
+
+    public static void initBoard(){
+        for(int i=1;i<10;i++)
+        {
+            boardArray[i]=' ';
+        }
+    }
+    public static void playerChoice()
+    {
+     Scanner scan = new Scanner(System.in);
+        System.out.println("Enter plyer choice ");
+     char choice=scan.next().charAt(0);
+
+    }
+    public static void main(String[] args) {
+        initBoard();
+
+    }
+
+}*/
